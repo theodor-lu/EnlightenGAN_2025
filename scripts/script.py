0293ec70214d89b77ac3d4e2000690e7d1b21786
Suraj Pai <surajballambat@gmail.com>
2020-05-29 06:14:53 +0200

    Simplify arguments for scripts

--- scripts/script.py
+++ scripts/script.py
@@ -4,12 +4,12 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("--port", type=str, default="8097")
 parser.add_argument("--train", action='store_true')
-parser.add_argument("--object", default='car', help='Object specific GAN training')
+parser.add_argument("--object", default='car', help='Mention object for Object specific GAN training')
 parser.add_argument("--predict", action='store_true')
-parser.add_argument("--name",  type=str, default="enlightening")
-parser.add_argument("--suffix",  type=str, default="test_dataset1")
-parser.add_argument("--gpu_ids",  type=str, default="0,1")
-parser.add_argument("--data_path",  type=str, default="/home/vq218944/Downloads/EnlightenGAN_Data")
+parser.add_argument("--name",  type=str, default="enlightening", help="For training, the name of the directory where checkpoints are created, for testing directory from which it is loaded")
+parser.add_argument("--gpu_ids",  type=str, default="0", help="GPU ids to use, to use 2 GPUs set to 0,1")
+parser.add_argument("--data_path",  type=str, default="/home/vq218944/Downloads/EnlightenGAN_Data", help="Path to the downloaded dataset")
+parser.add_argument("--loss_type",  type=str, default="stylefeat", help="Choose between relu5_1 | relu5_3 | stylefeat")
 
 opt = parser.parse_args()
 
@@ -17,7 +17,7 @@ if opt.train:
 	os.system("python train.py \
 		--dataroot {} \
 		--no_dropout \
-		--name enlightening \
+		--name {} \
 		--model single \
 		--dataset_mode unaligned \
 		--which_model_netG sid_unet_resize \
@@ -39,11 +39,11 @@ if opt.train:
         --times_residual \
 		--instance_norm 0 \
 		--vgg 1 \
-        --vgg_choose stylefeat \
+        --vgg_choose {} \
 		--gpu_ids 0,1 \
 		--resize_or_crop resize_and_crop \
 		--object {} \
-		--display_port={}".format(opt.data_path, opt.object, opt.port))
+		--display_port={}".format(opt.data_path, opt.name, opt.loss_type, opt.object, opt.port))
 
 elif opt.predict:
 	for i in range(1, 2):
